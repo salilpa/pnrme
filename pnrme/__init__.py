707b9d9174140213903149a550ac0ae54c082983
salil <salilpa007@gmail.com>
2014-07-01 20:20:02 +0530

    installed flask-bootstrap and dlask-debugtoolbar added a proper template for bootstrap

--- pnrme/__init__.py
+++ pnrme/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
+
+toolbar = DebugToolbarExtension(app)
+Bootstrap(app)
 
 from . import views
